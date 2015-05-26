07a76481a11d79e42d235b653b50b4da50b0ff28
Alexey Shiklomanov <alexey.shiklomanov@gmail.com>
2015-05-26 11:59:13 -0400

    Initilized submodule prezto

---
+++ setup.prezto.sh
@@ -0,0 +1,7 @@
+#!/bin/zsh
+
+# Create symbolic links
+setopt EXTENDED_GLOB
+for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
+  ln -s "$rcfile" "${ZDOTDIR:-$HOME}/${rcfile:t}"
+done
