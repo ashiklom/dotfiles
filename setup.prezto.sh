655cd363bcb147a04dd38f80d076e9e5a7cfe7c2
Alexey Shiklomanov (scc) <ashiklom@bu.edu>
2015-05-27 17:00:47 -0400

    Fixed prezto setup

--- setup.prezto.sh
+++ setup.prezto.sh
@@ -4,10 +4,10 @@
 setopt EXTENDED_GLOB
 ln -s ~/dotfiles/.zprezto ~/.zprezto
 ln -s ~/dotfiles/.zprezto/runcoms/.zlogin ~/.zlogin
-ln -s ~/dotfiles/.zprezto/runcoms/.zlogin ~/.zlogout
-ln -s ~/dotfiles/.zprezto/runcoms/.zlogin ~/.zpreztorc
-ln -s ~/dotfiles/.zprezto/runcoms/.zlogin ~/.zprofile
-ln -s ~/dotfiles/.zprezto/runcoms/.zlogin ~/.zshrc
+ln -s ~/dotfiles/.zprezto/runcoms/.zlogout ~/.zlogout
+ln -s ~/dotfiles/.zprezto/runcoms/.zpreztorc ~/.zpreztorc
+ln -s ~/dotfiles/.zprezto/runcoms/.zprofile ~/.zprofile
+ln -s ~/dotfiles/.zprezto/runcoms/.zshrc ~/.zshrc
 
 # for rcfile in ~/.zprezto/runcoms/^README.md; do
 #   ln -s "$rcfile" "~/${rcfile:t}"
