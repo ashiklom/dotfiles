d4bc911c626b6ad6d0b054f64467b715d91d709e
Alexey Shiklomanov <alexey.shiklomanov@gmail.com>
2015-06-05 10:18:03 -0400

    Added font type test

---
+++ tests/bolditalic.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Test terminal capability for modified fonts
+echo -e "Normal"
+echo -e "\e[1mBold\e[0m"
+echo -e "\e[2mDim\e[0m"
+echo -e "\e[3mItalic\e[0m"
+echo -e "\e[4mUnderlined\e[0m"
