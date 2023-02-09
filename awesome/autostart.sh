281f85b60ed4a87bfc469c49a48e00c81f840e6e
Alexey Shiklomanov <alexey.shiklomanov@gmail.com>
2023-02-09 10:29:56 -0500

    Remove unused dotfiles

--- awesome/autostart.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Disable tap to click
-synclient MaxTapTime=0 &
-nm-applet &
-#wicd-client -t &
-dropbox start
-
--- misc/touchpad-settings.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# Disable tap-to-click
-synclient MaxTapTime=0
